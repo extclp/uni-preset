3d95bdbdaa24dc92d2450aa76bc45b14e8c5de44
extclp <2842417407@qq.com>
2024-09-10 02:00:22 +0800

    update deps

--- src/shime-uni.d.ts
+++ src/shime-uni.d.ts
@@ -4,9 +4,3 @@ declare module "vue" {
   type Hooks = App.AppInstance & Page.PageInstance;
   interface ComponentCustomOptions extends Hooks {}
 }
-
-type A = import('@vue/runtime-core').GlobalComponents;
-
-declare module 'vue' {
-  interface GlobalComponents extends A {}
-}
